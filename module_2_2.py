595867a8de8d93157c55a9282eed0209f9330ac5
Mi10500 <crazybalu@gmail.com>
2024-12-20 23:42:18 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,12 @@
+
+number1 = int(input('Введите число 1: '))
+number2 = int(input('Введите число 2: '))
+number3 = int(input('Введите число 3: '))
+if number1 == number2 == number3:
+    print (3)
+elif number2 == number1 and number3  or number1 == number3:
+    print (2)
+elif number1 != number2 != number3:
+    print (0)
+
+
